e2d27e5a1239b613e8a31f83be9094063f6fce29
khaldon <mohamed.khaled33388@gmail.com>
2018-11-22 02:57:21 +0200

    editing

--- log_sign_app/models.py
+++ log_sign_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    username = models.CharField(blank=True, max_length=100)
+    email = models.EmailField()
+    re_email = models.EmailField()
+    password = models.CharField(blank=True, max_length=100)
+
+class LogIn(models.Model):
+    username = models.CharField(blank=True, max_length=100)
+    password = models.CharField(blank=True, max_length=100)
