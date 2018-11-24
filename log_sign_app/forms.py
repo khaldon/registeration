60e695f18798ebc67944201568d2b64f529f2f4f
khaldon <mohamed.khaled33388@gmail.com>
2018-11-24 23:48:46 +0200

    initial commit

--- log_sign_app/forms.py
+++ log_sign_app/forms.py
@@ -5,3 +5,15 @@ class FormSignUp(forms.ModelForm):
     class Meta():
         model = models.SignUp
         fields = ['username','email','password','re_password']
+        widgets = {
+        'password':forms.PasswordInput(),
+        're_password':forms.PasswordInput()
+        }
+
+class FormLogIn(forms.ModelForm):
+    class Meta():
+        model = models.LogIn
+        fields = ['username','password']
+        widgets = {
+        'password':forms.PasswordInput()
+        }
--- log_sign_app/views.py
+++ log_sign_app/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .forms import FormSignUp
+from django.http import HttpResponseRedirect,HttpResponse
+from django.contrib.auth.hashers import make_password
+from . import forms
+from . import models
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
 def get_sign_up(request):
-    if request.method == 'Post':
-        form = FormSignUp(method.POST)
+    if request.method == 'POST':
+        form = forms.FormSignUp(request.POST)
         if form.is_valid():
             username    = form.cleaned_data['username']
             email       = form.cleaned_data['email']
             password    = form.cleaned_data['password']
             re_password = form.cleaned_data['re_password']
-            print(username,email,password,re_password)
+            make_password(password)
+            form.save()
+
     else:
-        form = FormSignUp()
+        form = forms.FormSignUp()
     return render(request,'sign_up/sign_up.html',{'form':form})
 
 
 def log_in(request):
-    return render(request,'log_in/log_in.html')
+
+    if request.method == 'POST':
+        form = forms.FormLogIn(request.POST)
+        if form.is_valid():
+            username    = form.cleaned_data['username']
+            password    = form.cleaned_data['password']
+            data_get    = models.SignUp.objects.order_by('username')
+            for i in data_get:
+                if i.username == username and i.password == password:
+                    print("HEllo word")
+                else:
+                    print("Error in user")
+    else:
+        form = forms.FormLogIn()
+    return render(request,'log_in/log_in.html',{'form':form})
